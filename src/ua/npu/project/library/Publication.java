b403cb8e9095fe931271dbc86571458a5d6a43b4
PopovaAI <niceant16@mail.ru>
2016-02-02 10:23:20 +0200

    Publication
    Add publication

--- src/ua/npu/project/library/Publication.java
+++ src/ua/npu/project/library/Publication.java
@@ -4,4 +4,49 @@ package ua.npu.project.library;
 
 public class Publication {
     //властивості: назва, рік, автор
+    
+    protected String name;
+    protected int year;
+    protected String author;
+    
+    public Publication;
+    this.name="";
+    this.year=0;
+    this.autor="";
+    
+    public Publication (String name, int year, String author){
+        
+        this.name=name;
+        this.year=year;
+        this.author=author;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName (String name){
+        this.name=name
+    }
+    
+   public int getYear(){
+        return year;
+    }
+    
+    public void setYear (int year){
+        this.year=year
+    }
+        
+        public String getAuthor(){
+        return author;
+    }
+    
+    public void setAuthor (String author){
+        this.author=author
+}
+
+@Override
+public String toString(){
+    return "Publication{"+"name='"+name+'\''+",year="+year+",author="+autor+'\''+ '}';
+}
 }
